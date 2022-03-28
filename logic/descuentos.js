40404130c09445e04bf2873e2f0e38434959428a
camlo01 <camilobeltran174@gmail.com>
2022-03-28 16:38:06 -0500

    se agrega la logica y funcionalidad para los cupones y validar su existencia y permitir ejecutar el descuento

--- logic/descuentos.js
+++ logic/descuentos.js
@@ -8,12 +8,25 @@ function calcularDescuento(precio, descuento) {
   //   return "El producto con valor de " + precio + " Es de " + precioFinal;
 }
 
+// array de cupones
+let cupones = ["1", "cupon1", "cupon2", "cupon3"];
+
+function verificarExistenciaCupon(cupon) {
+  if (cupones.includes(cupon)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //--------------------------------------------------------------
 //---------------- Conectar JS con HTML ------------------------
 //--------------------------------------------------------------
 
 function buttonCalcularDescuento() {
-  console.log("Se ejecuta buttonCalcularDescuento() que calcula el descuento del precio de un producto");
+  console.log(
+    "Se ejecuta buttonCalcularDescuento() que calcula el descuento del precio de un producto"
+  );
   const inputPrecio = document.getElementById("precio");
   const inputDescuento = document.getElementById("descuento");
   const precio = inputPrecio.value;
@@ -21,6 +34,22 @@ function buttonCalcularDescuento() {
   const resultado = calcularDescuento(precio, descuento);
   const parrafoPantalla = document.getElementById("resultadoEnPantalla");
   parrafoPantalla.innerText = resultado;
+}
+
+function buttonUsarCupon() {
+  console.log("Se ejecuta botón que calcula el precio con el cupon ingresado");
+  const inputCupon = document.getElementById("cupon");
+  const inputDescuento = document.getElementById("descuento");
+  const descuento = inputDescuento.value;
+
+  const cupon = inputCupon.value;
+
+  if (verificarExistenciaCupon(cupon)) {
+    return buttonCalcularDescuento(descuento);
+  } else {
+    // return "El cupon ingresado no es valido";
+    return alert("El cupon ingresado no es valido");
+  }
 }
 
 console.groupEnd();
