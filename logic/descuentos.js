4ba07a696a9071af970f2c905f9c945c0642c847
camlo01 <camilobeltran174@gmail.com>
2022-03-28 14:50:22 -0500

    Se implemento la logica, con la coleccion con html

--- logic/descuentos.js
+++ logic/descuentos.js
@@ -4,7 +4,7 @@ console.group("Lógica de descuentos");
 function calcularDescuento(precio, descuento) {
   const porcentajeDescuento = 100 - descuento;
   const precioFinal = (precio * porcentajeDescuento) / 100;
-  return "El precio final del producto es de " + precioFinal;
+  return "El precio final del producto es de: $" + precioFinal;
   //   return "El producto con valor de " + precio + " Es de " + precioFinal;
 }
 
@@ -13,11 +13,14 @@ function calcularDescuento(precio, descuento) {
 //--------------------------------------------------------------
 
 function buttonCalcularDescuento() {
+  console.log("Se ejecuta buttonCalcularDescuento() que calcula el descuento del precio de un producto");
   const inputPrecio = document.getElementById("precio");
   const inputDescuento = document.getElementById("descuento");
   const precio = inputPrecio.value;
-  const descuento = inputPrecio.value;
-
-  return;
+  const descuento = inputDescuento.value;
+  const resultado = calcularDescuento(precio, descuento);
+  const parrafoPantalla = document.getElementById("resultadoEnPantalla");
+  parrafoPantalla.innerText = resultado;
 }
+
 console.groupEnd();
